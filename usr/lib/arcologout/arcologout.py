e4588d3037072c60e9c6f900103cfefc36162e7d
Erik Dubois <erik.dubois@gmail.com>
2020-05-02 16:43:15 +0200

    Hour and Minute pm gives problems in other languages as Japanese

--- usr/lib/arcologout/arcologout.py
+++ usr/lib/arcologout/arcologout.py
@@ -22,7 +22,7 @@ class TransparentWindow(Gtk.Window):
     cmd_restart = "systemctl reboot"
     cmd_suspend = "systemctl suspend"
     cmd_hibernate = "systemctl hibernate"
-    cmd_lock = 'betterlockscreen -l dimblur -- --timestr="%I:%M%P"'
+    cmd_lock = 'betterlockscreen -l dimblur -- --timestr="%H:%M"'
     wallpaper = "/usr/share/arcologout/wallpaper.jpg"
     d_buttons = ['cancel',
                  'shutdown',
