be66cf2759dbfa159512bfd427a854f7f311bab9
arcolinuxz <arcolinuxinfo@gmail.com>
2021-12-11 12:56:55 +0100

    for ly

--- usr/share/arcologout/Functions.py
+++ usr/share/arcologout/Functions.py
@@ -113,9 +113,26 @@ def get_config(self, Gdk, Gtk, config):
 
 
 def _get_logout():
-    out = subprocess.run(["sh", "-c", "env | grep DESKTOP_SESSION"],
+    desktop = "unknown"
+    try:
+        out = subprocess.run(["sh", "-c", "env | grep DESKTOP_SESSION"],
                          shell=False, stdout=subprocess.PIPE)
-    desktop = out.stdout.decode().split("=")[1].strip()
+        desktop = out.stdout.decode().split("=")[1].strip()
+        desktop = desktop.lower()
+    except Exception as e:
+        desktop = "unknown"
+
+    # in case display manager ly is active
+
+    status = os.system('systemctl is-active --quiet ly')
+    if status == 0:
+        try:
+            out = subprocess.run(["sh", "-c", "env | grep XDG_CURRENT_DESKTOP"],
+                             shell=False, stdout=subprocess.PIPE)
+            desktop = out.stdout.decode().split("=")[1].strip()
+            desktop = desktop.lower()
+        except Exception as e:
+            desktop = "unknown"
 
     print("Your desktop is " + desktop)
     if desktop in ("herbstluftwm", "/usr/share/xsessions/herbstluftwm"):
@@ -172,7 +189,7 @@ def _get_logout():
     elif desktop in ("gnome-classic", "/usr/share/xsessions/gnome-classic"):
         return "gnome-session-quit --logout --no-prompt"
     elif desktop :
-        return "pkill -KILL -u $USER"
+        return "pkill awesome | pkill bspwm | pkill cwm |  pkill dwm | pkill dusk | pkill fvwm3 | pkill herbstluftwm | pkill i3 | pkill icewm | pkill jwm | pkill leftwm | pkill lxqt | pkill openbox | pkill qtile | pkill spectrwm | pkill wmderland | pkill xmonad"
     return None
 
 def button_active(self, data, GdkPixbuf):
