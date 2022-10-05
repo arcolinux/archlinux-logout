b15134e37e5541b42080d91a536fb7efc4496c40
Suchith Sridhar <suchithsridhar@gmail.com>
2022-10-05 00:01:51 -0300

    created an install script to allow user to install just the logout screen directly

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This installation is only for the archlinux-logout
+# this isn't an official script and this works for me
+# I cannot guarantee that it's going to work for you.
+
+cp -r usr/share/archlinux-logout usr/share/archlinux-logout-themes /usr/share/
+cp usr/bin/archlinux-logout /usr/bin/
