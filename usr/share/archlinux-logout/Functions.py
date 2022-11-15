cd1b3faf47adacdaeea0a2f81eca72d6d8d6a365
arcolinuxz <arcolinuxinfo@gmail.com>
2022-11-15 12:26:26 +0100

    update

--- usr/share/archlinux-logout/Functions.py
+++ usr/share/archlinux-logout/Functions.py
@@ -194,6 +194,8 @@ def _get_logout():
         return "pkill stumpwm"
     elif desktop in ("leftwm", "/usr/share/xsessions/leftwm"):
         return "pkill leftwm"
+    elif desktop in ("hypr", "/usr/share/xsessions/hypr"):
+        return "pkill Hypr"
     elif desktop in ("dusk", "/usr/share/xsessions/dusk"):
         return "pkill dusk"
     elif desktop in ("wmderland", "/usr/share/xsessions/wmderland"):
@@ -205,7 +207,7 @@ def _get_logout():
     elif desktop in ("gnome-classic", "/usr/share/xsessions/gnome-classic"):
         return "gnome-session-quit --logout --no-prompt"
     elif desktop :
-        return "pkill awesome | pkill bspwm | pkill cwm |  pkill dwm | pkill dusk | pkill fvwm3 | pkill herbstluftwm | pkill i3 | pkill icewm | pkill jwm | pkill leftwm | pkill lxqt | pkill openbox | pkill qtile | pkill spectrwm | pkill wmderland | pkill xmonad | pkill worm | pkill berry"
+        return "pkill awesome | pkill bspwm | pkill cwm |  pkill dwm | pkill dusk | pkill fvwm3 | pkill herbstluftwm | pkill i3 | pkill icewm | pkill jwm | pkill leftwm | pkill lxqt | pkill openbox | pkill qtile | pkill spectrwm | pkill wmderland | pkill xmonad | pkill worm | pkill berry | pkill Hypr | pkill hypr"
     return None
 
 def button_active(self, data, GdkPixbuf):
